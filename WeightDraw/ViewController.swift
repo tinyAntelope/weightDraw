9e6c2822468d48f538c94fe0d25729708042fd68
tinyAntelope <scott@tinyantelope.com>
2015-03-22 10:39:24 -0400

    Initial Commit

---
+++ WeightDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeightDraw
+//
+//  Created by THE MAYBERS on 3/22/15.
+//  Copyright (c) 2015 Tiny Antelope Publishing, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
